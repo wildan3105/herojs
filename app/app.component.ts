c7d8729f38ebe29a8753c26b7d0c69d61496d835
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-11 20:31:00 +0700

    some adds

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component }    from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1> {{title}} </h1>
+    <my-heroes> </my-heroes>
+  `
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,10 @@
+// import angular js module
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
 import { AppComponent }         from './app.component';
+
+// import built in module
 import { HeroDetailComponent }  from './hero-detail.component';
 
 @NgModule({
