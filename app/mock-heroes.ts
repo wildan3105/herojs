c0e00a62183ccd7def596907a954bb67369f35d6
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-12 02:03:21 +0700

    add some fictional heroes, just for fun :D

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,9 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {id: 1, name: 'Wildan'},
-  {id: 2, name: 'Syahrun'},
-  {id: 3, name: 'Nahar'},
-  {id: 4, name: 'Ahmad'},
-  {id: 5, name: 'Dhani'}
+  {id: 1, name: 'Superman'},
+  {id: 2, name: 'Batman'},
+  {id: 3, name: 'Spider-Man'},
+  {id: 4, name: 'Thor'},
+  {id: 5, name: 'Hal Jordan'},
+  {id: 6, name: 'Wonder Woman'},
+  {id: 7, name: 'Captain America'},
+  {id: 8, name: 'Martian Manhunter'},
+  {id: 9, name: 'Hulk'},
+  {id: 10, name: 'Arrow'}
 ];
