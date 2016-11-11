728155e563c9775fadf7aaf85b944772bcf0eab4
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-11 20:48:54 +0700

    add router-link

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,8 @@ import { Component }    from '@angular/core';
   selector: 'my-app',
   template: `
     <h1> {{title}} </h1>
-    <my-heroes> </my-heroes>
+    <a routerLink="/heroes"> Heroes</a>
+    <router-outlet></router-outlet>
   `
 })
 
