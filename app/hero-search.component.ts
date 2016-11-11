16c2f97c39a8768f97750939f2d2fcd2af8b49c6
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-12 03:38:33 +0700

    remove copyright

--- app/hero-search.component.ts
+++ app/hero-search.component.ts
@@ -47,10 +47,3 @@ export class HeroSearchComponent implements OnInit {
     this.router.navigate(link);
   }
 }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
