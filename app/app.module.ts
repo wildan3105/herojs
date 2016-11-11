2db180793f04f62de716856a92245e1215e16589
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-11 20:35:12 +0700

    initial routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,20 @@
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
-import { AppComponent }         from './app.component';
+import { RouterModule }         from '@angular/router';
 
 // import built in module
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
+
+RouterModule.forRoot({
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+})
 
 @NgModule({
   imports:      [
