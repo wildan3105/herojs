e5247781b69657e181ec8dc80084b1a70a63a58f
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-12 01:16:22 +0700

    update some routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,6 @@
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
-import { RouterModule }         from '@angular/router';
 
 // import built in module
 import { AppComponent }         from './app.component';
@@ -10,32 +9,14 @@ import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
-
+import { AppRoutingModule }     from './app-routing.module';
 
 
 @NgModule({
   imports:      [
       BrowserModule,
       FormsModule,
-      RouterModule.forRoot({
-        {
-          path: 'detail/:id',
-          component: HeroDetailComponent
-        },
-        {
-          path: '',
-          redirectTo: '/dashboard',
-          pathMatch: 'full'
-        },
-        {
-          path: 'heroes',
-          component: HeroesComponent
-        },
-        {
-          path: 'dashboard',
-          component: DashboardComponent
-        },
-      })
+      AppRoutingModule
     ],
   declarations: [
       AppComponent ,
@@ -43,10 +24,8 @@ import { DashboardComponent }   from './dashboard.component';
       HeroesComponent,
       DashboardComponent
     ],
-  providers: [
-    HeroService
-  ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
