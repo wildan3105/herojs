2fb2f483171b209f619e9399047fb59fa38566c0
Wildan S. Nahar <wildansnahar@gmail.com>
2016-11-13 16:53:17 +0700

    add tags

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      {id: 1, name: 'Superman'},
-      {id: 2, name: 'Batman'},
-      {id: 3, name: 'Spider-Man'},
-      {id: 4, name: 'Thor'},
-      {id: 5, name: 'Hal Jordan'},
-      {id: 6, name: 'Wonder Woman'},
-      {id: 7, name: 'Captain America'},
-      {id: 8, name: 'Martian Manhunter'},
-      {id: 9, name: 'Hulk'},
-      {id: 10, name: 'Arrow'}
+      {id: 1, name: 'Superman', tag: 'DC'},
+      {id: 2, name: 'Batman', tag: 'Marvell'},
+      {id: 3, name: 'Spider-Man', tag: 'DC'},
+      {id: 4, name: 'Thor', tag: 'Marvell'},
+      {id: 5, name: 'Hal Jordan', tag: 'DC'},
+      {id: 6, name: 'Wonder Woman', tag: 'Marvell'},
+      {id: 7, name: 'Captain America', tag: 'DC'},
+      {id: 8, name: 'Martian Manhunter', tag: 'Marvell'},
+      {id: 9, name: 'Hulk', tag: 'DC'},
+      {id: 10, name: 'Arrow', tag: 'Marvell'}
     ];
     return {heroes};
   }
